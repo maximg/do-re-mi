439d0b7c6d482575042f01f1ea3ea26a47c3e48c
maximg <maxim.golov@gmail.com>
2016-12-17 08:42:19 +0100

    Moved finalize method to Meta

--- src/do-re-mi.cpp
+++ src/do-re-mi.cpp
@@ -8,16 +8,28 @@ struct TransformedData { double value; };
  
 InputData produceFx() { return InputData{42}; }
 TransformedData transformFx(InputData input) { return TransformedData{(double)input.value}; }
-std::string finalizeFx(TransformedData input) { return std::to_string(input.value); }
+
+
+template<typename TResult, typename TInput, typename TMeta>
+std::function<TResult(TInput)> getFinalizer(TMeta) {
+    throw std::runtime_error("getFinalizer must be specialized");
+}
 
 template<typename TContent>
 struct Meta {
-    std::string meta;
+    using TMeta = std::string;
+    TMeta meta;
     TContent content;
 
     template<typename TResult>
     Meta<TResult> transform(std::function<TResult(TContent)> fx) {
         return Meta<TResult>{meta, fx(content)};
+    }
+
+    template<typename TResult>
+    TResult finalize() {
+        const auto finalizer = getFinalizer<TResult, TContent, TMeta>(meta);
+        return finalizer(content);
     }
 };
 
@@ -25,15 +37,19 @@ Meta<InputData> producer() {
     return Meta<InputData>{"meta", produceFx()};
 }
 
-std::string finalizer(Meta<TransformedData> input) {
-    return std::string("[") + input.meta + "]: " + finalizeFx(input.content); 
+template<>
+std::function<std::string(TransformedData)>
+getFinalizer<std::string, TransformedData, Meta<TransformedData>::TMeta>(Meta<TransformedData>::TMeta meta) {
+    return [meta](TransformedData content){
+        return std::string("[") + meta + "]: " + std::to_string(content.value);
+    };
 }
 
 int main(int argc, char **argv)
 {
     Meta<InputData> input = producer();
     Meta<TransformedData> transformed = input.transform<TransformedData>(transformFx);
-    std::string output = finalizer(transformed);
+    std::string output = transformed.finalize<std::string>();
     std::cout << "Output: " << output << "\n";
     return 0;
 }
