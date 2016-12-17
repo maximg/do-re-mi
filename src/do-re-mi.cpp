87c5937778850072fef7a130daf8f72a0458b865
maximg <maxim.golov@gmail.com>
2016-12-17 07:57:22 +0100

    Added Meta wrapper

--- src/do-re-mi.cpp
+++ src/do-re-mi.cpp
@@ -5,14 +5,32 @@
 struct InputData { int value; };
 struct TransformedData { double value; };
  
-InputData producer() { return InputData{42}; }
-TransformedData transformer(InputData input) { return TransformedData{(double)input.value}; }
-std::string finalizer(TransformedData input) { return std::to_string(input.value); }
- 
+InputData produceFx() { return InputData{42}; }
+TransformedData transformFx(InputData input) { return TransformedData{(double)input.value}; }
+std::string finalizeFx(TransformedData input) { return std::to_string(input.value); }
+
+template<typename TContent>
+struct Meta {
+    std::string meta;
+    TContent content;
+};
+
+Meta<InputData> producer() {
+    return Meta<InputData>{"meta", produceFx()};
+}
+
+Meta<TransformedData> transformer(Meta<InputData> input) {
+    return Meta<TransformedData>{input.meta, transformFx(input.content)};
+}
+
+std::string finalizer(Meta<TransformedData> input) {
+    return std::string("[") + input.meta + "]: " + finalizeFx(input.content); 
+}
+
 int main(int argc, char **argv)
 {
-    InputData input = producer();
-    TransformedData transformed = transformer(input);
+    Meta<InputData> input = producer();
+    Meta<TransformedData> transformed = transformer(input);
     std::string output = finalizer(transformed);
     std::cout << "Output: " << output << "\n";
     return 0;
